ba8d9d6ca699514d646ef14d34342a21acec71d8
fullstackjam <fullstackjam@outlook.com>
2023-11-07 21:51:51 +0800

    comment nlp part

--- predict_intent.py
+++ predict_intent.py
@@ -1,21 +1,21 @@
-from transformers import BertForSequenceClassification, BertTokenizer
+# from transformers import BertForSequenceClassification, BertTokenizer
 
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+# tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
-model = BertForSequenceClassification.from_pretrained(
-    "models/my_trained_model_get_weather"
-)
-
-
-def predict_intent(message):
-    inputs = tokenizer(message, return_tensors="pt")
-    outputs = model(**inputs)
-    predictions = outputs.logits.argmax(dim=-1).item()
-    return predictions
+# model = BertForSequenceClassification.from_pretrained(
+#     "models/my_trained_model_get_weather"
+# )
 
 
-def get_intent_name(user_message):
-    intent = predict_intent(user_message)
+# def predict_intent(message):
+#     inputs = tokenizer(message, return_tensors="pt")
+#     outputs = model(**inputs)
+#     predictions = outputs.logits.argmax(dim=-1).item()
+#     return predictions
 
-    intent_mapping = {0: "weather", 1: "joke", 2: "greeting"}
-    return intent_mapping[intent]
+
+# def get_intent_name(user_message):
+#     intent = predict_intent(user_message)
+
+#     intent_mapping = {0: "weather", 1: "joke", 2: "greeting"}
+#     return intent_mapping[intent]
